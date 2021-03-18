ec86d6452d713b5e4b6394bfc1d1dcefc2c77e76
Zhurkin_Alex <lexus1812141@gmail.com>
2021-03-18 17:11:54 +0300

    reles zhopa ups

--- routes/pictures.js
+++ routes/pictures.js
@@ -16,14 +16,14 @@ router.get('/new', (req,res)=>{
 router.post("/new", async function (req, res, next) {
   
   let filedata = req.file;
-  // console.log(filedata);
+  console.log(filedata);
 
   if(!filedata) {
 
     res.send("Ошибка при загрузке файла");
   } else {
     let src1 = `/uploads/${req.file.filename}`
-    // console.log(req.file.filename);
+    console.log(req.file.filename);
     console.log(src1);
     res.render('pictures/new', {src1})
   }
