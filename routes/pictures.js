9a00de45f34ac16ebb956c663e14fe2924bc2323
Zhurkin_Alex <lexus1812141@gmail.com>
2021-03-18 18:34:42 +0300

    for Alla

--- routes/pictures.js
+++ routes/pictures.js
@@ -3,6 +3,8 @@ const Grandma = require('../models/grandma')
 const Son = require('../models/son')
 const Pic = require('../models/pic')
 
+const { createWorker } = require ('tesseract');
+
 router.get('/', (req, res) => {
   const { user } = req.session
   res.render('pictures/pictures', user)
@@ -15,14 +17,28 @@ router.get('/new', (req, res) => {
 router.post("/new", async function (req, res, next) {
 
   let filedata = req.file;
-  console.log(filedata);
-
+  // console.log(filedata);
+ 
   if (!filedata) {
 
     res.send("Ошибка при загрузке файла");
   } else {
     let src1 = `/uploads/${req.file.filename}`
-    console.log(req.file.filename);
+    // console.log(req.file.filename);
+    
+    const{user} = req.session
+    
+   const babka = await Grandma.findOne({email: user.email})
+   console.log(babka);
+
+    const newPic =  await Pic.create ({
+      src: src1,
+      author: user
+    })
+
+    babka.pics.push(newPic)
+    await babka.save()
+
     res.render('pictures/new', { src1 })
   }
 });
